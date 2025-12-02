bb4da4decf6eabc7d94fc1b47d41e8335b5b02d7
Rémi Grimault <remigrimault@Remis-MacBook-Air.local>
2025-12-02 16:02:09 +0100

    docs: removed comments for cc

--- LabyrinthTest/Build/KeyMasterTest.cs
+++ LabyrinthTest/Build/KeyMasterTest.cs
@@ -12,33 +12,27 @@ public class KeyMasterTest
     [Test]
     public void Dispose_WithNoDoorsOrKeys_DoesNotThrow()
     {
-        // Arrange
         var keymaster = new Keymaster();
 
-        // Act & Assert
         Assert.DoesNotThrow(() => keymaster.Dispose());
     }
 
     [Test]
     public void Dispose_WithMatchedDoorAndKey_DoesNotThrow()
     {
-        // Arrange
         var keymaster = new Keymaster();
         keymaster.NewDoor();
         keymaster.NewKeyRoom();
 
-        // Act & Assert
         Assert.DoesNotThrow(() => keymaster.Dispose());
     }
 
     [Test]
     public void Dispose_WithUnplacedDoor_ThrowsInvalidOperationException()
     {
-        // Arrange
         var keymaster = new Keymaster();
         keymaster.NewDoor();
 
-        // Act & Assert
         var ex = Assert.Throws<InvalidOperationException>(() => keymaster.Dispose());
         Assert.That(ex?.Message, Is.EqualTo("Unmatched key/door creation"));
     }
@@ -46,11 +40,9 @@ public class KeyMasterTest
     [Test]
     public void Dispose_WithUnplacedKeyRoom_ThrowsInvalidOperationException()
     {
-        // Arrange
         var keymaster = new Keymaster();
         keymaster.NewKeyRoom();
 
-        // Act & Assert
         var ex = Assert.Throws<InvalidOperationException>(() => keymaster.Dispose());
         Assert.That(ex?.Message, Is.EqualTo("Unmatched key/door creation"));
     }
@@ -58,13 +50,11 @@ public class KeyMasterTest
     [Test]
     public void Dispose_WithMultipleUnmatchedDoorsAndKeys_ThrowsInvalidOperationException()
     {
-        // Arrange
         var keymaster = new Keymaster();
         keymaster.NewDoor();
         keymaster.NewDoor();
         keymaster.NewKeyRoom();
 
-        // Act & Assert
         var ex = Assert.Throws<InvalidOperationException>(() => keymaster.Dispose());
         Assert.That(ex?.Message, Is.EqualTo("Unmatched key/door creation"));
     }
@@ -76,13 +66,10 @@ public class KeyMasterTest
     [Test]
     public void NewDoor_CreatesLockedDoor()
     {
-        // Arrange
         var keymaster = new Keymaster();
 
-        // Act
         var door = keymaster.NewDoor();
 
-        // Assert
         Assert.That(door, Is.Not.Null);
         Assert.That(door.IsLocked, Is.True);
         Assert.That(door.IsTraversable, Is.False);
@@ -91,14 +78,11 @@ public class KeyMasterTest
     [Test]
     public void NewDoor_WithExistingKeyRoom_PlacesKeyInRoom()
     {
-        // Arrange
         var keymaster = new Keymaster();
         var keyRoom = keymaster.NewKeyRoom();
 
-        // Act
         var door = keymaster.NewDoor();
 
-        // Assert
         using var all = Assert.EnterMultipleScope();
         Assert.That(door.IsLocked, Is.True);
         Assert.That(keyRoom.Pass().HasItems, Is.True);
@@ -108,15 +92,12 @@ public class KeyMasterTest
     [Test]
     public void NewDoor_Multiple_CreatesMultipleLockedDoors()
     {
-        // Arrange
         var keymaster = new Keymaster();
 
-        // Act
         var door1 = keymaster.NewDoor();
         var door2 = keymaster.NewDoor();
         var door3 = keymaster.NewDoor();
 
-        // Assert
         using var all = Assert.EnterMultipleScope();
         Assert.That(door1.IsLocked, Is.True);
         Assert.That(door2.IsLocked, Is.True);
@@ -130,13 +111,10 @@ public class KeyMasterTest
     [Test]
     public void NewKeyRoom_CreatesEmptyRoom()
     {
-        // Arrange
         var keymaster = new Keymaster();
 
-        // Act
         var room = keymaster.NewKeyRoom();
 
-        // Assert
         Assert.That(room, Is.Not.Null);
         Assert.That(room.Pass().HasItems, Is.False);
     }
@@ -144,14 +122,11 @@ public class KeyMasterTest
     [Test]
     public void NewKeyRoom_WithExistingDoor_ReceivesKey()
     {
-        // Arrange
         var keymaster = new Keymaster();
         var door = keymaster.NewDoor();
 
-        // Act
         var keyRoom = keymaster.NewKeyRoom();
 
-        // Assert
         using var all = Assert.EnterMultipleScope();
         Assert.That(keyRoom.Pass().HasItems, Is.True);
         Assert.That(keyRoom.Pass().ItemTypes.First(), Is.EqualTo(typeof(Key)));
@@ -161,15 +136,12 @@ public class KeyMasterTest
     [Test]
     public void NewKeyRoom_Multiple_CreatesMultipleRooms()
     {
-        // Arrange
         var keymaster = new Keymaster();
 
-        // Act
         var room1 = keymaster.NewKeyRoom();
         var room2 = keymaster.NewKeyRoom();
         var room3 = keymaster.NewKeyRoom();
 
-        // Assert
         using var all = Assert.EnterMultipleScope();
         Assert.That(room1, Is.Not.Null);
         Assert.That(room2, Is.Not.Null);
@@ -183,14 +155,11 @@ public class KeyMasterTest
     [Test]
     public void NewDoorThenNewKeyRoom_KeyIsPlacedInRoom()
     {
-        // Arrange
         var keymaster = new Keymaster();
 
-        // Act
         var door = keymaster.NewDoor();
         var keyRoom = keymaster.NewKeyRoom();
 
-        // Assert
         using var all = Assert.EnterMultipleScope();
         Assert.That(door.IsLocked, Is.True);
         Assert.That(keyRoom.Pass().HasItems, Is.True);
@@ -200,14 +169,11 @@ public class KeyMasterTest
     [Test]
     public void NewKeyRoomThenNewDoor_KeyIsPlacedInRoom()
     {
-        // Arrange
         var keymaster = new Keymaster();
 
-        // Act
         var keyRoom = keymaster.NewKeyRoom();
         var door = keymaster.NewDoor();
 
-        // Assert
         using var all = Assert.EnterMultipleScope();
         Assert.That(door.IsLocked, Is.True);
         Assert.That(keyRoom.Pass().HasItems, Is.True);
@@ -217,10 +183,8 @@ public class KeyMasterTest
     [Test]
     public void MultipleDoorsBeforeKeys_AllKeysArePlacedCorrectly()
     {
-        // Arrange
         var keymaster = new Keymaster();
 
-        // Act
         var door1 = keymaster.NewDoor();
         var door2 = keymaster.NewDoor();
         var door3 = keymaster.NewDoor();
@@ -228,7 +192,6 @@ public class KeyMasterTest
         var keyRoom2 = keymaster.NewKeyRoom();
         var keyRoom3 = keymaster.NewKeyRoom();
 
-        // Assert
         using var all = Assert.EnterMultipleScope();
         Assert.That(door1.IsLocked, Is.True);
         Assert.That(door2.IsLocked, Is.True);
@@ -241,10 +204,8 @@ public class KeyMasterTest
     [Test]
     public void MultipleKeysBeforeDoors_AllKeysArePlacedCorrectly()
     {
-        // Arrange
         var keymaster = new Keymaster();
 
-        // Act
         var keyRoom1 = keymaster.NewKeyRoom();
         var keyRoom2 = keymaster.NewKeyRoom();
         var keyRoom3 = keymaster.NewKeyRoom();
@@ -252,7 +213,6 @@ public class KeyMasterTest
         var door2 = keymaster.NewDoor();
         var door3 = keymaster.NewDoor();
 
-        // Assert
         using var all = Assert.EnterMultipleScope();
         Assert.That(door1.IsLocked, Is.True);
         Assert.That(door2.IsLocked, Is.True);
@@ -265,10 +225,8 @@ public class KeyMasterTest
     [Test]
     public void InterleavedDoorsAndKeys_AllKeysArePlacedCorrectly()
     {
-        // Arrange
         var keymaster = new Keymaster();
 
-        // Act
         var door1 = keymaster.NewDoor();
         var keyRoom1 = keymaster.NewKeyRoom();
         var keyRoom2 = keymaster.NewKeyRoom();
@@ -276,7 +234,6 @@ public class KeyMasterTest
         var door3 = keymaster.NewDoor();
         var keyRoom3 = keymaster.NewKeyRoom();
 
-        // Assert
         using var all = Assert.EnterMultipleScope();
         Assert.That(door1.IsLocked, Is.True);
         Assert.That(door2.IsLocked, Is.True);
@@ -289,17 +246,14 @@ public class KeyMasterTest
     [Test]
     public void KeyCanOpenCorrespondingDoor()
     {
-        // Arrange
         var keymaster = new Keymaster();
         var door = keymaster.NewDoor();
         var keyRoom = keymaster.NewKeyRoom();
         var playerInventory = new MyInventory();
 
-        // Act
         playerInventory.MoveItemFrom(keyRoom.Pass());
         var canOpen = door.Open(playerInventory);
 
-        // Assert
         using var all = Assert.EnterMultipleScope();
         Assert.That(canOpen, Is.True);
         Assert.That(door.IsOpened, Is.True);
@@ -309,12 +263,10 @@ public class KeyMasterTest
     [Test]
     public void FiveDoorsAndFiveKeys_AllMatch()
     {
-        // Arrange
         var keymaster = new Keymaster();
         var doors = new Door[5];
         var keyRooms = new Room[5];
 
-        // Act
         for (int i = 0; i < 5; i++)
         {
             doors[i] = keymaster.NewDoor();
@@ -324,7 +276,6 @@ public class KeyMasterTest
             keyRooms[i] = keymaster.NewKeyRoom();
         }
 
-        // Assert
         using var all = Assert.EnterMultipleScope();
         for (int i = 0; i < 5; i++)
         {
@@ -341,7 +292,6 @@ public class KeyMasterTest
     [Test]
     public void UsingStatement_WithBalancedDoorsAndKeys_DisposesSuccessfully()
     {
-        // Arrange & Act & Assert
         Assert.DoesNotThrow(() =>
         {
             using var keymaster = new Keymaster();
@@ -353,7 +303,6 @@ public class KeyMasterTest
     [Test]
     public void UsingStatement_WithUnmatchedDoor_ThrowsOnDispose()
     {
-        // Arrange & Act & Assert
         var ex = Assert.Throws<InvalidOperationException>(() =>
         {
             using var keymaster = new Keymaster();
